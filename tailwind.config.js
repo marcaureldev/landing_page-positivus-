5f2fb629759485d4b7952b37b380335c35526803
marcaureldev <ahouandjinoumarcaurel10@gmail.com>
2024-03-06 06:21:16 +0100

    Second Commit (Positivus)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/ @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src//*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'tesk': ['Space Grotesk', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
